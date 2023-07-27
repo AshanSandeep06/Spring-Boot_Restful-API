722783c35fa3a232f1569aabfcb6b47137e8cd1c
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-27 16:47:00 +0530

    Refactored SignUp Controller

--- src/main/java/lk/epic/restfulAPI/controller/SignUpController.java
+++ src/main/java/lk/epic/restfulAPI/controller/SignUpController.java
@@ -1,6 +1,11 @@
 package lk.epic.restfulAPI.controller;
 
+import lk.epic.restfulAPI.dto.SignUpDTO;
+import lk.epic.restfulAPI.service.SignUpService;
+import lk.epic.restfulAPI.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/signup")
 @CrossOrigin
 public class SignUpController {
+    @Autowired
+    private SignUpService signUpService;
 
+    public ResponseUtil signUp(@RequestBody SignUpDTO signUpDTO) {
+        return signUpService.signUp(signUpDTO);
+    }
 }
