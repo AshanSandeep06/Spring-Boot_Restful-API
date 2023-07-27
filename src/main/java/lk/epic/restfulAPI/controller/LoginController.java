29954baf53e8762754a710ea62e6895fec317cd1
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-27 16:48:52 +0530

    Refactored Login Controller

--- src/main/java/lk/epic/restfulAPI/controller/LoginController.java
+++ src/main/java/lk/epic/restfulAPI/controller/LoginController.java
@@ -1,6 +1,11 @@
 package lk.epic.restfulAPI.controller;
 
+import lk.epic.restfulAPI.dto.LoginDTO;
+import lk.epic.restfulAPI.service.LoginService;
+import lk.epic.restfulAPI.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/login")
 @CrossOrigin
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
 
+    public ResponseUtil login(@RequestBody LoginDTO loginDTO) {
+        return loginService.login(loginDTO);
+    }
 }
