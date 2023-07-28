577431d59b96834777038b3cec42eb8b759f8c91
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-28 16:01:49 +0530

    Content Updated in JWT Authentication Filter class

--- src/main/java/lk/epic/restfulAPI/config/filter/JWTAuthenticationFilter.java
+++ src/main/java/lk/epic/restfulAPI/config/filter/JWTAuthenticationFilter.java
@@ -1,7 +1,9 @@
-package lk.epic.restfulAPI.filter;
+package lk.epic.restfulAPI.config.filter;
 
+import lk.epic.restfulAPI.config.service.JwtService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,20 +16,31 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor // Give this Constructor for, final fields (eg:- private final String myName)
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         // Implement JWT Authentication Filter
         final String authenticationHeader = request.getHeader("Authorization");
         final String jwt_token;
+        final String email;
 
         if (authenticationHeader == null || authenticationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
         } else {
+            // Extract the JWT Token from Authentication Header
             jwt_token = authenticationHeader.substring(7);
             System.out.println(jwt_token);
 
+            // After Extracting the JWT Token, Then I want to extract
+            // also this User Email
 
+            // We need to extract User Email from the JWT Token,
+            // We need a class that can manipulate this JWT Token
+            // And we need to pass JWT Token as a parameter
+            // To JWTService
+            email = jwtService.extractEmail(jwt_token);
         }
     }
 }
---
+++ src/main/java/lk/epic/restfulAPI/config/service/JwtService.java
@@ -0,0 +1,9 @@
+package lk.epic.restfulAPI.config.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service // To tell Spring, create a bean from this class
+public class JwtService {
+
+}
