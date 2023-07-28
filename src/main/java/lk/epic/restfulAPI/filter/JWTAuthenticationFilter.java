60efa10362d8e54da55b7ea1323d3e038484562a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-28 14:53:16 +0530

    Initiated JWTAuthentication Filter class to intercept HTTP Requests

---
+++ src/main/java/lk/epic/restfulAPI/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package lk.epic.restfulAPI.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor // Give this Constructor for, final fields (eg:- private final String myName)
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Implement JWT Authentication Filter
+    }
+}
