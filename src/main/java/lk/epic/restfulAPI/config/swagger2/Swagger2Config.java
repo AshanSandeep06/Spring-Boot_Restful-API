3f08a64771d6db4fe175a7a970c4e86dad0ed3aa
AshanSandeep06 <ashansandeep06@gmail.com>
2023-08-04 14:52:36 +0530

    Content Updated

--- src/main/java/lk/epic/restfulAPI/config/swagger2/Swagger2Config.java
+++ src/main/java/lk/epic/restfulAPI/config/swagger2/Swagger2Config.java
@@ -2,7 +2,7 @@ package lk.epic.restfulAPI.config.swagger2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,12 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-public class Swagger2Config {
+public class Swagger2Config implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("movie-api")
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -28,16 +26,16 @@ public class Swagger2Config {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Your API Documentation")
-                .description("Description of your API")
+                .title("My API")
+                .description("API documentation for my Spring Boot application")
                 .licenseUrl("user123@gmail.com")
-                .version("1.0")
+                .version("1.0.0")
                 .build();
     }
 
-    /*@Override
+   /* @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
+        registry.addResourceHandler("/swagger-ui/index.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
--- src/main/java/lk/epic/restfulAPI/controller/MovieController.java
+++ src/main/java/lk/epic/restfulAPI/controller/MovieController.java
@@ -2,11 +2,11 @@ package lk.epic.restfulAPI.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lk.epic.restfulAPI.dto.MovieDTO;
 import lk.epic.restfulAPI.service.MovieService;
 import lk.epic.restfulAPI.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,20 +20,22 @@ import javax.validation.constraints.Size;
 @RestController
 @RequestMapping("/api/v1/movie")
 @CrossOrigin
-@Api(value = "Movie Controller", protocols = "http")
+@Api(value = "Movie Controller", tags = "Movies Controller APIs")
 @Validated
 public class MovieController {
     @Autowired
     private MovieService movieService;
 
     @GetMapping
-    @ApiOperation(value = "Get All Movies", response = ResponseEntity.class, code = 200)
+    @ApiOperation(value = "Get All Movies", notes = "Get All Movies")
+    @ApiResponse(code = 200, message = "Success", response = ResponseUtil.class)
     public ResponseUtil getAllMovies() {
         return movieService.getAllMovies();
     }
 
     @GetMapping(path = "/{imdb}")
-    @ApiOperation(value = "Get Movie by Imdb", response = ResponseEntity.class, code = 200)
+    @ApiOperation(value = "Get Movie by Imdb", notes = "Get Movie by Imdb")
+    @ApiResponse(code = 200, message = "Success", response = ResponseUtil.class)
     public ResponseUtil getMovieByIMDB(@Valid @NotEmpty @NotBlank
                                        @Size(min = 9, max = 9, message = "Imdb should be fixed with 9 characters")
                                        @Pattern(regexp = "^(tt)[0-9]{7}$", message = "Imdb should be in this pattern eg:-tt0371743, tt0371769")
@@ -42,21 +44,24 @@ public class MovieController {
     }
 
     @PostMapping(path = "/add")
-    @ApiOperation(value = "Add a New Movie", response = ResponseEntity.class, code = 200)
+    @ApiOperation(value = "Save a New Movie", notes = "Save a New Movie")
+    @ApiResponse(code = 200, message = "Success", response = ResponseUtil.class)
     public ResponseUtil addMovie(@Valid @RequestBody MovieDTO movieDTO) {
         return movieService.addMovie(movieDTO);
     }
 
     @PutMapping(path = "/update")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ApiOperation(value = "Update a Movie", response = ResponseEntity.class, code = 200)
+    @ApiOperation(value = "Update a Movie", notes = "Update a Movie")
+    @ApiResponse(code = 200, message = "Success", response = ResponseUtil.class)
     public ResponseUtil updateMovie(@Valid @RequestBody MovieDTO movieDTO) {
         return movieService.updateMovie(movieDTO);
     }
 
     @DeleteMapping(path = "/delete/{imdb}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ApiOperation(value = "Delete a Movie", response = ResponseEntity.class, code = 200)
+    @ApiOperation(value = "Delete a Movie", notes = "Delete a Movie")
+    @ApiResponse(code = 200, message = "Success", response = ResponseUtil.class)
     public ResponseUtil deleteMovie(@Valid @NotEmpty @NotBlank
                                     @Size(min = 9, max = 9, message = "Imdb should be fixed with 9 characters")
                                     @Pattern(regexp = "^(tt)[0-9]{7}$", message = "Imdb should be in this pattern eg:-tt0371743, tt0371769")
